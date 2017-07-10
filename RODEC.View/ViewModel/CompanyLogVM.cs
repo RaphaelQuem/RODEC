e5c57831399e03dad4d0bf1c9437854dbdb78b97
Raphael Maia <raphzmaia@gmail.com>
2017-07-10 12:06:14 -0300

    -Connection bug fixed

---
+++ RODEC.View/ViewModel/CompanyLogVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RODEC.View.ViewModel
+{
+    public class CompanyLogVM
+    {
+        public string Code;
+        public List<string> Logs;
+    }
+}
---
+++ RODEC.View/ViewModel/MonitorVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RODEC.View.ViewModel
+{
+    public class MonitorVM
+    {
+        //public Dictionary<string>;
+    }
+}
